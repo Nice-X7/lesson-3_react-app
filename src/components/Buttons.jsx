7ada2cdc45d81cbdb5fa2475acf282eaf28a450c
Nice_X <777soer@gmail.com>
2023-11-25 21:27:53 +0300

    counter!

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,9 @@
-export const Buttons = () => {
+export const Buttons = ({increace, decreace, reset}) => {
     return(
         <div className="buttons">
-            <button className="blue">Увеличить</button>
-            <button className="orange">Уменьшить</button>
-            <button className="gray">Сбросить</button>
+            <button className="blue" onClick={increace}>Увеличить</button>
+            <button className="orange" onClick={decreace}>Уменьшить</button>
+            <button className="gray" onClick={reset}>Сбросить</button>
         </div>
     )
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react"
 import {Buttons} from "./Buttons"
 
 export const Section = () => {
+    const [number, setChangeNumber] = useState(0)
+
+    const increace = () => {
+            setChangeNumber(number + 3)
+    }
+
+    const decreace = () => {
+        if (number > 0) {
+            setChangeNumber(number - 2)
+        } else if (number < 0) {
+            setChangeNumber(0)
+        }
+    }
+
+    const reset = () => {
+        setChangeNumber(0)
+    }
+
     return(
         <section>
             <div className="zero">
-                0
+                {number}
             </div>
 
-            <Buttons />
+            <Buttons increace={increace} decreace={decreace} reset={reset}/>
         </section>
     )
 }
